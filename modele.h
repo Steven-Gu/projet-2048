367b242c1466e152497de5714d02cb99df348405
Tianwen <stevengu2019@hotmail.com>
2021-12-04 00:42:14 +0100

    Avec documentation

--- modele.h
+++ modele.h
@@ -1,23 +1,76 @@
-
-#ifndef MODELE_H
-#define MODELE_H
+#ifndef MODELE_H__
+#define MODELE_H__
 #include <vector>
 using namespace std;
 const int GAUCHE = 7, DROITE = 4, HAUT = 8, BAS = 2;
-typedef vector<vector<int> > Plateau;
+typedef vector<vector<int>> Plateau;
 struct PlateauScore {
 	Plateau plateau;
 	int score;
 };
+/** Des fonctions peuvent être ajoutés à ce fichier si besoin est (fonctions pour mettre à jour le score par exemple)
+ **/
+
+/** génère un Plateau de dimensions 4*4 ne contenant que des 0
+ *  @return un Plateau vide
+ **/
 Plateau plateauVide();
+
+/** génère deux nombres sur des cases aléatoires d'un Plateau vide
+ *  @return un Plateau en début de jeu
+ **/
 Plateau plateauInitial();
+
+/** génère aléatoirement un 2 ou un 4 avec des probabilités respectives de 9/10 et 1/10
+ *  @return 2 ou 4
+ **/
 int tireDeuxOuQuatre();
+
+/** déplace les tuiles d'un Plateau vers la gauche et les combine si possible
+ *  @param plateau le Plateau
+ *  @return le Plateau une fois déplacé vers la gauche
+ **/
 PlateauScore deplacementGauche(PlateauScore plateau);
+
+/** déplace les tuiles d'un Plateau vers la droite et les combine si possible
+ *  @param plateau le Plateau
+ *  @return le Plateau une fois déplacé vers la droite
+ **/
 PlateauScore deplacementDroite(PlateauScore plateau);
-PlateauScore deplacementHaut(PlateauScore plateau); 
+
+/** déplace les tuiles d'un Plateau vers le haut et les combine si possible
+ *  @param plateau le Plateau
+ *  @return le Plateau une fois déplacé vers le haut
+ **/
+PlateauScore deplacementHaut(PlateauScore plateau);
+
+/** déplace les tuiles d'un Plateau vers le bas et les combine si possible
+ *  @param plateau le Plateau
+ *  @return le Plateau une fois déplacé vers le bas
+ **/
 PlateauScore deplacementBas(PlateauScore plateau);
+
+/** déplace les tuiles d'un Plateau dans la direction donnée et génère une nouvelle tuile si le déplacement est valide
+ *  @param plateau le Plateau
+ *  @param direction la direction
+ *  @return le Plateau déplacé dans la direction
+ **/
 PlateauScore deplacement(PlateauScore plateau, int direction);
+
+/** affiche un Plateau
+ * @param p le Plateau
+ **/
 string dessine(Plateau g);
+
+/** permet de savoir si une partie est terminée
+ *  @param plateau un Plateau
+ *  @return true si le plateau est vide, false sinon
+ **/
 bool estTermine(PlateauScore plateau);
+
+/** permet de savoir si une partie est gagnée
+ * @param plateau un Plateau
+ * @return true si le plateau contient un 2048, false sinon
+ **/
 bool estGagnant(Plateau plateau);
 #endif
